93bf30aa239e586e338452c0a495d0ab251ecbec
Alexandre Werneck <xanwerneck@gmail.com>
2021-06-24 04:18:12 -0300

    change test folder and extension definition

--- backend/__tests__/event.spec.ts
+++ backend/__tests__/event.spec.ts
@@ -6,37 +6,37 @@ type TypeEvent = {
 }
 
 describe('I need to make sure the endpoint event is providing data in the right way.', () => {
-  it('only 10 rows', () => {
-    return request(app)
+  it('only 10 rows', async () => {
+    await request(app)
       .get('/event/0')
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows.length).toEqual(10)
       });
   })
-  it('no rows - number is greater than the number of events', () => {
-    return request(app)
+  it('no rows - number is greater than the number of events', async () => {
+    await request(app)
       .get('/event/500000')
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows).toEqual([])
       });
   })
-  it('negative page', () => {
-    return request(app)
+  it('negative page', async () => {
+    await request(app)
       .get('/event/-1')
       .expect(400)
   })
-  it('no events for invalid id', () => {
-    return request(app)
+  it('no events for invalid id', async () => {
+    await request(app)
       .get('/event/XXX$$$$/0')
       .expect(200)
       .expect(function(res) {
         expect(res.body.count).toBe(0)
       })
   })
-  it('event is for a family', () => {
-    return request(app)
+  it('event is for a family', async () => {
+    await request(app)
       .get('/event/ad3512a6-91b1-4d7d-a005-6f8764dd0111/0')
       .expect(200)
       .expect(function(res) {
