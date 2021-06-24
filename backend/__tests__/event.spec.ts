d92e58091ccdc25f7d9f3e49821a157b761db891
Alexandre Werneck <xanwerneck@gmail.com>
2021-06-24 04:49:21 -0300

    define a done function at the end of each code

--- backend/__tests__/event.spec.ts
+++ backend/__tests__/event.spec.ts
@@ -6,36 +6,40 @@ type TypeEvent = {
 }
 
 describe('I need to make sure the endpoint event is providing data in the right way.', () => {
-  it('only 10 rows', async () => {
+  it('only 10 rows', async (done) => {
     await request(app)
       .get('/event/0')
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows.length).toEqual(10)
       });
+    done()
   })
-  it('no rows - number is greater than the number of events', async () => {
+  it('no rows - number is greater than the number of events', async (done) => {
     await request(app)
       .get('/event/500000')
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows).toEqual([])
       });
+    done()
   })
-  it('negative page', async () => {
+  it('negative page', async (done) => {
     await request(app)
       .get('/event/-1')
       .expect(400)
+    done()
   })
-  it('no events for invalid id', async () => {
+  it('no events for invalid id', async (done) => {
     await request(app)
       .get('/event/XXX$$$$/0')
       .expect(200)
       .expect(function(res) {
         expect(res.body.count).toBe(0)
       })
+    done()
   })
-  it('event is for a family', async () => {
+  it('event is for a family', async (done) => {
     await request(app)
       .get('/event/ad3512a6-91b1-4d7d-a005-6f8764dd0111/0')
       .expect(200)
@@ -43,5 +47,6 @@ describe('I need to make sure the endpoint event is providing data in the right 
         expect(res.body.rows.length).toBeGreaterThan(0)
         res.body.rows.map((row : TypeEvent) => expect(row.care_recipient_id).toBe('ad3512a6-91b1-4d7d-a005-6f8764dd0111'))
       })
+    done()
   })
 });
--- backend/__tests__/ping.spec.ts
+++ backend/__tests__/ping.spec.ts
@@ -2,12 +2,13 @@ import app from '../src/application'
 import * as request from 'supertest';
 
 describe('We are grateful to you for doing this it.', () => {
-  it('thanks you', async () => {
+  it('thanks you', async (done) => {
     await request(app)
       .get('/hello')
       .expect(200)
       .expect(function(res) {
         expect(res.body.greetings).toContain('Thank you');
       });
+    done()
   })
 });
