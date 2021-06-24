bb326789ece2aa2d0834fba9560a552e01837696
Alexandre Werneck <xanwerneck@gmail.com>
2021-06-24 05:01:29 -0300

    define a timeout to prevent long time for testing

--- backend/__tests__/event.spec.ts
+++ backend/__tests__/event.spec.ts
@@ -9,6 +9,7 @@ describe('I need to make sure the endpoint event is providing data in the right 
   it('only 10 rows', async (done) => {
     await request(app)
       .get('/event/0')
+      .timeout(10000)
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows.length).toEqual(10)
@@ -18,6 +19,7 @@ describe('I need to make sure the endpoint event is providing data in the right 
   it('no rows - number is greater than the number of events', async (done) => {
     await request(app)
       .get('/event/500000')
+      .timeout(10000)
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows).toEqual([])
@@ -27,12 +29,14 @@ describe('I need to make sure the endpoint event is providing data in the right 
   it('negative page', async (done) => {
     await request(app)
       .get('/event/-1')
+      .timeout(10000)
       .expect(400)
     done()
   })
   it('no events for invalid id', async (done) => {
     await request(app)
       .get('/event/XXX$$$$/0')
+      .timeout(10000)
       .expect(200)
       .expect(function(res) {
         expect(res.body.count).toBe(0)
@@ -42,6 +46,7 @@ describe('I need to make sure the endpoint event is providing data in the right 
   it('event is for a family', async (done) => {
     await request(app)
       .get('/event/ad3512a6-91b1-4d7d-a005-6f8764dd0111/0')
+      .timeout(10000)
       .expect(200)
       .expect(function(res) {
         expect(res.body.rows.length).toBeGreaterThan(0)
--- backend/__tests__/ping.spec.ts
+++ backend/__tests__/ping.spec.ts
@@ -5,6 +5,7 @@ describe('We are grateful to you for doing this it.', () => {
   it('thanks you', async (done) => {
     await request(app)
       .get('/hello')
+      .timeout(5000)
       .expect(200)
       .expect(function(res) {
         expect(res.body.greetings).toContain('Thank you');
